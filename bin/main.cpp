edc850f462ac89be94fc7234ed926be20ec6afc1
boscher <enzo.boscher@etu.u-pec.fr>
2020-01-21 15:09:33 +0100

    gestion des erreurs d'entrees

--- bin/main.cpp
+++ bin/main.cpp
@@ -13,42 +13,62 @@ int main(int argc, char** argv) {
     bool e = false;
     bool t = false;
     string dotfile;
+    string temps = "-1";
     if(argc==1) {
         raise_error("[options] <filename.log>\n \toptions:\n\t\t-g <filename.dot>\n\t\t-e\n\t\t-t <heure>\nuse man analog for more informations");
         return 0;
     }
     int i = 1;
-    while(i != argv) {
+    while(i != argc) {
         if(argv[i][0] == '-') {
             if(argv[i][1] == 'g') {
-                regex dot {"^.*\\.dot"};
-                if(argc >= i+1 ) {
-                    if(!regex_match(argv[i+1],dot)) {
-                        raise_error("-g <filename.dot> <filename.log>\n\t\tFile needs to have .dot extension");
-                        return 21;
+                if(!g) {    
+                    regex dot {"^.*\\.dot"};
+                    if(argc >= i+2 ) {
+                        if(!regex_match(argv[i+1],dot)) {
+                            raise_error("-g <filename.dot> <filename.log>\n\t\tFile needs to have .dot extension");
+                            return 21;
+                        }
+                    } else {
+                        raise_error("-g <filename.dot> <filename.log>\n\t\t-g needs a file with the .dot extension");
+                        return 0;
                     }
+                    g = true;
+                    i++;
+                    dotfile = argv[i];
                 } else {
-                    raise_error("-g <filename.dot> <filename.log\n\t\t-g needs a file with the .dot extension");
+                    raise_error("-g <filename.dot> <filename.log>\n\t\tyou can't use -g twice");
+                    return 0;
                 }
-                g = true;
-                i++;
-                dotfile = argv[i];
             } else if(argv[i][1] == 'e') {
-                e = true;
-            } else if(argv[i][1] == 't') {
-                if(argc >= i+1 ) {
-                    regex number {"[[:digit:]]+"};
-                    if(!regex_match(argv[i+1],number)) {
-                        raise_error("-t <hour> <filename.log>\n\t\tHour needs to be a number");
-                        return 25;
-                    } else if (atoi(argv[i+1] < 0 || atoi(argv[i+1] > 23 )) {
-                        raise_error("-t <hour> <filename.log\n\t\tHour needs to be between 0 & 23");
-                        return 28;
-                    }
-
+                if(!e) {
+                    e = true;
                 } else {
-                    raise_error("-g <hour> <filename.log\n\t\t-t needs an hour");
-                    return 29;
+                    raise_error("-e <filename.log>\n\t\tyou can't use -e twice");
+                    return 0;
+                }
+            } else if(argv[i][1] == 't') {
+                if(!t) {    
+                    if(argc >= i+2 ) {
+                        regex number {"[[:digit:]]+"};
+                        if(!regex_match(argv[i+1],number)) {
+                            raise_error("-t <hour> <filename.log>\n\t\tHour needs to be a positive number");
+                            return 25;
+                        } else if (atoi(argv[i+1]) > 23 ) {
+                            raise_error("-t <hour> <filename.log\n\t\tHour needs to be between 0 & 23");
+                            return 28;
+                        }
+                        t = true;
+                        i++;
+                        temps = argv[i];
+                
+                    } else {
+                        raise_error("-t <hour> <filename.log>\n\t\t-t needs an hour");
+                        return 29;
+                    }
+                } else {
+                    raise_error("-t <hour> <filename.log>\n\t\tyou can't use -t twice");
+                    return 0;
                 }
             } else {
                 raise_error("options accepted:\n\t\t-g <filename.dot>\n\t\t-e\n\t\t-t <heure>\nuse man analog for more informations");
@@ -57,4 +77,10 @@ int main(int argc, char** argv) {
         }
         i++;
     }
+    regex log {"^.*\\.log"};
+    if(!regex_match(argv[i-1],log)) {
+        raise_error("<filename.log> expected\n\t\tVerify your file has the right extension");
+        return 0;
+    }
+    return 0;
 }
