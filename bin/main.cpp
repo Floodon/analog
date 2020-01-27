5ce30cf7a649801d0156d9c34ce6bb769157a340
Floodon <39013778+Floodon@users.noreply.github.com>
2020-01-27 15:55:31 +0100

    Adding error numbers

--- bin/main.cpp
+++ bin/main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char** argv) {
     string temps = "-1";
     if(argc==1) {
         raise_error("[options] <filename.log>\n \toptions:\n\t\t-g <filename.dot>\n\t\t-e\n\t\t-t <heure>");
-        return 0;
+        return 20;
     }
     int i = 1;
     while(i != argc) {
@@ -28,25 +28,25 @@ int main(int argc, char** argv) {
                     if(argc >= i+3 ) {
                         if(!regex_match(argv[i+1],dot)) {
                             raise_error("-g <filename.dot> <filename.log>\n\t\tFile needs to have .dot extension");
-                            return 21;
+                            return 25;
                         }
                     } else {
                         raise_error("-g <filename.dot> <filename.log>\n\t\t-g needs a file with the .dot extension");
-                        return 0;
+                        return 24;
                     }
                     g = true;
                     i++;
                     dotfile_str = argv[i];
                 } else {
                     raise_error("-g <filename.dot> <filename.log>\n\t\tyou can't use -g twice");
-                    return 0;
+                    return 23;
                 }
             } else if(argv[i][1] == 'e') {
                 if(!e) {
                     e = true;
                 } else {
                     raise_error("-e <filename.log>\n\t\tyou can't use -e twice");
-                    return 0;
+                    return 26;
                 }
             } else if(argv[i][1] == 't') {
                 if(!t) {    
@@ -54,10 +54,10 @@ int main(int argc, char** argv) {
                         regex number {"[[:digit:]]+"};
                         if(!regex_match(argv[i+1],number)) {
                             raise_error("-t <hour> <filename.log>\n\t\tHour needs to be a positive number");
-                            return 25;
+                            return 29;
                         } else if (atoi(argv[i+1]) > 23 ) {
                             raise_error("-t <hour> <filename.log\n\t\tHour needs to be between 0 & 23");
-                            return 28;
+                            return 30;
                         }
                         t = true;
                         i++;
@@ -65,29 +65,32 @@ int main(int argc, char** argv) {
                 
                     } else {
                         raise_error("-t <hour> <filename.log>\n\t\t-t needs an hour");
-                        return 29;
+                        return 28;
                     }
                 } else {
                     raise_error("-t <hour> <filename.log>\n\t\tyou can't use -t twice");
-                    return 0;
+                    return 27;
                 }
             } else {
                 raise_error("options accepted:\n\t\t-g <filename.dot>\n\t\t-e\n\t\t-t <heure>");
                 return 22;
             }
-        }
+        } else {
+	    raise_error("options accepted:\n\t\t-g <filename.dot>\n\t\t-e\n\t\t-t <heure>\n\t\tPlease don't forget to input a '-' before the option");
+	    return 21;
+	}
         i++;
     }
     i--;
     regex log {"^.*\\.log"};
     if(!regex_match(argv[i],log)) {
-        raise_error("<filename.log> expected\n\t\tVerify your file has the right extension");
-        return 0;
+        raise_error("<filename.log> expected\n\t\tVerify your log file has the .log extension");
+        return 31;
     }
     ifstream logfile(argv[i]);
     if(!logfile) {
-        raise_error("<filename.log>\n\t\tError when opening the file");
-        return 0;
+        raise_error("<filename.log>\n\t\tError when opening the log file");
+        return 32;
     }
 	Catalogue c;
 	c.Charger(logfile,e,temps);
@@ -102,8 +105,8 @@ int main(int argc, char** argv) {
 	if(g){
        ofstream dotfile(dotfile_str.c_str());
         if(!dotfile) {
-            raise_error("<filename.dot>\n\t\tError when opening the file");
-            return 0;
+            raise_error("<filename.dot>\n\t\tError when opening the dot file");
+            return 33;
         }else{
 		cout << "Dot-file " << dotfile_str << " generated" << endl;
 		c.GraphViz(dotfile);
